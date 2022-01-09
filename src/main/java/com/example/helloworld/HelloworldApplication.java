8a58fcb5c27e7386ad0b4ccd560bd837ce4447e2
lixiangping <admin>
2022-01-09 11:12:51 +0800

    Initial commit

---
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+    public static void main(String[] args) {
+        System.out.println("启动啦啦啦啦啦啦~~~~~~~~~~~~~~~~~");
+        System.out.println("启动啦啦啦啦啦啦~~~~~~~~~~~~~~~~~");
+        System.out.println("启动啦啦啦啦啦啦~~~~~~~~~~~~~~~~~");
+        System.out.println("启动啦啦啦啦啦啦~~~~~~~~~~~~~~~~~");
+        System.out.println("启动啦啦啦啦啦啦~~~~~~~~~~~~~~~~~");
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+}
