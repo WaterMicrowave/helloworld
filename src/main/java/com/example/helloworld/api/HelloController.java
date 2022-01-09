c8142378c67266c10391448dc429bacaab8ba5ff
lixiangping <admin>
2022-01-10 00:27:40 +0800

    commit

--- src/main/java/com/example/helloworld/api/HelloController.java
+++ src/main/java/com/example/helloworld/api/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "你好，Jenkins！CI的优势在于自动化打包、构建和部署项目！";
+        return "你好，Jenkins！CI的优势在于自动化打包、构建和部署项目！<h1>相较于传统人工打包、上传、发布节省了很多人力和时间成本。是不是很香~~</h1>";
     }
 
 }
